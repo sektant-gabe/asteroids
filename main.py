1cbe4e57a134878fdf888477f4ef3ec7c9dfed02
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-07-04 13:26:41 -0300

    B: day 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
-digital_timer = 0
+dt = 0
 
 # sets player position to be on the middle of the screen
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -33,16 +33,16 @@ while running:
     # RENDER YOUR GAME HERE
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * digital_timer
+        player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * digital_timer
+        player_pos.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * digital_timer
+        player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * digital_timer
+        player_pos.x += 300 * dt
 
 
-    digital_timer = clock.tick(60) / 1000 # limits fps to 60 and splits into miliseconds
+    dt = clock.tick(60) / 1000 # limits fps to 60 and splits into miliseconds
 
     # if player_pos.x >= 1260:
     #     player_pos.x = 1260
