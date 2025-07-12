d58bd164ae581294a46ecb698ff2661a8dcfc5a1
sektant <sektant@sektants-MacBook-Pro.local>
2025-07-12 15:52:27 -0300

    few changes

--- shot.py
+++ shot.py
@@ -7,7 +7,7 @@ class Shot(CircleShape):
         super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screem):
-        pygame.draw.circle(screem, "white", self.position, self.radius, 3)
+        pygame.draw.circle(screem, "white", self.position, self.radius)
 
     def update(self, dt):
         self.position += self.velocity * dt
